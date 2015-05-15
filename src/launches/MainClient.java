60097d4e111f5a3de62ee62447eae8f705231084
RomainGaillard <romain.gaillard@y-nov.com>
2015-05-15 14:25:38 +0200

    MAJ de Valentin
    Signed-off-by: RomainGaillard <romain.gaillard@y-nov.com>

--- src/launches/MainClient.java
+++ src/launches/MainClient.java
@@ -3,13 +3,16 @@ package launches;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import connectors.Player;
 import view.SalonView;
+import view.ConnexionView;
 
 public class MainClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SalonView s = new SalonView();
+		new ConnexionView();
 	}
 
 }
