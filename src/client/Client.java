7a1315fb5a37f5abf2a68d1407da0e1ce43854c6
RomainGaillard <romain.gaillard@y-nov.com>
2015-05-15 23:48:43 +0200

    Correction d'une erreur qui empêcher la connexion à une room ou la création de room après l'appel à la méthode listRoom.
    Signed-off-by: RomainGaillard <romain.gaillard@y-nov.com>

--- src/client/Client.java
+++ src/client/Client.java
@@ -17,23 +17,36 @@ import connectors.Room;
 
 public class Client{
 
+	private String ip; 
+	private int port;
 	private Socket sock; 
 	private InputStreamReader isr; 
 	private BufferedReader in;
 	private Player player;
 	
 	public Client(String ip, int port){
+		this.ip = ip; 
+		this.port = port;
+	}
+	
+	public void InitConnexionServer()
+	{
 		try {
+			if(sock != null)
+			{
+				sock.close();
+				in.close();	
+			}	
 			sock = new Socket(ip,port);
 			isr = new InputStreamReader(sock.getInputStream());
 			in = new BufferedReader(isr);
 			in.ready();
-			System.out.println("Ouverture client");
 		} catch (IOException e) {System.out.println("Erreur Client:"+e.getMessage());}
 	}
 	
 	public Player ConnexionRoom(String namePlayer, int idRoom)
 	{
+		InitConnexionServer();
 		player = null;
 		try{
 			JSONObject dataset = new JSONObject();
@@ -54,6 +67,7 @@ public class Client{
 	
 	public Player CreateRoom(String namePlayer, String nameRoom, int NbPlayerRoom)
 	{
+		InitConnexionServer();
 		player = null; 
 		try{
 			JSONObject dataset = new JSONObject();
@@ -75,6 +89,7 @@ public class Client{
 	
 	public ArrayList<Room> ListRoom()
 	{
+		InitConnexionServer();
 		ArrayList<Room> lRoom = new ArrayList<Room>();
 		try{
 			JSONObject dataset = new JSONObject();
